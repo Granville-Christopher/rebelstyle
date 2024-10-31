a962a79f905fd73bb664f7e901d068a67d35e6f6
chucky-golden <chukschibyke92@gmail.com>
2024-10-31 07:42:33 +0100

    'collections'

--- controllers/basicController.js
+++ controllers/basicController.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c28a698d1b033e9d6198fe139c953f03c063da862b188971ec624b1cd3ad1d4b
-size 26665
+oid sha256:00afd9de2577c9073445ecef5449b1a73ee704e04146d1f65c0ea1ac1fb19322
+size 26888
