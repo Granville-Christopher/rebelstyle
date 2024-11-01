1856b75e30026112ab6e94751675b2b020aa7abe
chucky-golden <chukschibyke92@gmail.com>
2024-11-01 07:20:18 +0100

    'collections'

--- routes/basicRoute.js
+++ routes/basicRoute.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:677ef4b8fce3b25695e78ae55f13b2d3e873b2f6c7c26859c5ffeeae43f82b80
-size 1788
+oid sha256:b3fffcffe58a1e073235a63aec89185134200acd8a59115bc1a30a6c77de5bf6
+size 1878
--- routes/sellerRoute.js
+++ routes/sellerRoute.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4c118e9fb659c72cf77f08fccbfc288f93393d7a0e49a21634440a4aa4a6558a
-size 2147
+oid sha256:ea5695b91136c7fa6b0dd86fb0c38ad510e85d66ab33881adb7992b3e567aee9
+size 2236
