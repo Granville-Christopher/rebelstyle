f9d8a4e83d2bb209d80a133acda09aad74dd3ba3
chucky-golden <chukschibyke92@gmail.com>
2024-11-01 15:02:34 +0100

    'sellers'

--- controllers/sellerController.js
+++ controllers/sellerController.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:342cce25882cf04f43dd2f9e4a93fb4893f440cebc4d9a594195f02dd59e2499
-size 27798
+oid sha256:1820935186da3f394f863fe5781106f700cec4f19ac2ba13b4b947c2c51153dc
+size 33473
--- routes/sellerRoute.js
+++ routes/sellerRoute.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ea5695b91136c7fa6b0dd86fb0c38ad510e85d66ab33881adb7992b3e567aee9
-size 2236
+oid sha256:e13b6554f14f8f88d0ae55d0aa4abfe9a8b38ecdcf7b8a2ffdf15981ecf7f094
+size 2490
